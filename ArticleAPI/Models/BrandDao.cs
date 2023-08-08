3b9922f815a6dfad737f581f51bffa491cf49fa5
Fabricio Di Paola <fabriciodamiandipaola@gmail.com>
2023-08-08 18:28:01 -0300

    BrandDao-Atrinuttes-Created

---
+++ ArticleAPI/Models/BrandDao.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DB;
+
+namespace ArticleAPI.Models
+{
+    public class BrandDao
+    {
+        public BrandDao() { data = new DataAccess(); }
+        public DataAccess data;
+
+    }
+}
