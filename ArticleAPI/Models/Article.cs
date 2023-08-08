5cb14ee0eca626c1ad3d720e24bb46f3b0a310c4
Fabricio Di Paola <fabriciodamiandipaola@gmail.com>
2023-08-08 18:24:08 -0300

    Article-Class-Completed

---
+++ ArticleAPI/Models/Article.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ArticleAPI.Models
+{
+    public class Article
+    {
+        public Article() { 
+            brand= new Brand();
+            category= new Category();
+            gama = new Gama();
+        }
+        public int id { get; set; }
+        public string Model { get; set; }
+        public Brand brand { get; set; }
+        public string nro_Model { get; set; }
+        public Category category { get; set; }
+        public string description { get; set; }
+        public string url_Image { get; set; }
+        public Gama gama { get; set; }
+    }
+}
