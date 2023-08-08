14385cf02e64c36d90c263ecd6d897fd500fe74c
Fabricio Di Paola <fabriciodamiandipaola@gmail.com>
2023-08-08 18:39:12 -0300

    Category-Atributtes-Completed

---
+++ ArticleAPI/Models/CategoryDao.cs
@@ -0,0 +1,14 @@
+﻿using DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ArticleAPI.Models
+{
+    public class CategoryDao
+    {
+        public CategoryDao() { data = new DataAccess(); }
+        public DataAccess data;
+    }
+}
