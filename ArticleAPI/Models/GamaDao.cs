e99b71fbffbe4d31b6f17819abbdd7b0e992c285
Fabricio Di Paola <fabriciodamiandipaola@gmail.com>
2023-08-08 18:46:47 -0300

    GamaDao-Atributtes-Created

---
+++ ArticleAPI/Models/GamaDao.cs
@@ -0,0 +1,14 @@
+﻿using DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ArticleAPI.Models
+{
+    public class GamaDao
+    {
+        public GamaDao() { data = new DataAccess(); }
+        public DataAccess data;
+    }
+}
