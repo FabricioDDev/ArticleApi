b34c1174cbeac5b04c024f0e84a5cffc2207a5e1
Fabricio Di Paola <fabriciodamiandipaola@gmail.com>
2023-08-08 19:01:36 -0300

    GamaDao-get_Gamas-Function-Completed

--- ArticleAPI/Models/GamaDao.cs
+++ ArticleAPI/Models/GamaDao.cs
@@ -10,5 +10,25 @@ namespace ArticleAPI.Models
     {
         public GamaDao() { data = new DataAccess(); }
         public DataAccess data;
+
+        public List<Gama> get_Gamas()
+        {
+            List<Gama> list = new List<Gama>();
+            try
+            {
+                data.Query("SELECT Id, Name FROM Gama_Table;");
+                data.Read();
+                while (data.readerProp.Read())
+                {
+                    Gama aux_Gama = new Gama();
+                    aux_Gama.Id = (int)data.readerProp["Id"];
+                    aux_Gama.Name = (string)data.readerProp["Name"];
+                    list.Add(aux_Gama);
+                }
+                return list;
+            }
+            catch (Exception ex) { throw ex; }
+            finally { data.Close(); }
+        }
     }
 }
