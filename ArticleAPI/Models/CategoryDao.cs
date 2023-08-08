f693d55d8f50fac509606aa933ba9c66d69516c7
Fabricio Di Paola <fabriciodamiandipaola@gmail.com>
2023-08-08 18:42:13 -0300

    CategoryDao-get_CategoriesFunction-Completed

--- ArticleAPI/Models/CategoryDao.cs
+++ ArticleAPI/Models/CategoryDao.cs
@@ -10,5 +10,24 @@ namespace ArticleAPI.Models
     {
         public CategoryDao() { data = new DataAccess(); }
         public DataAccess data;
+        public List<Category> get_Categories()
+        {
+            List<Category> list = new List<Category>();
+            try
+            {
+                data.Query("SELECT Id, Name FROM Category_Table;");
+                data.Read();
+                while (data.readerProp.Read())
+                {
+                    Category aux_Category = new Category();
+                    aux_Category.Id = (int)data.readerProp["Id"];
+                    aux_Category.Name = (string)data.readerProp["Name"];
+                    list.Add(aux_Category);
+                }
+                return list;
+            }
+            catch (Exception ex) { throw ex; }
+            finally { data.Close(); }
+        }
     }
 }
