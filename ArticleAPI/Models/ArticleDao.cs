af36b55cc574df845cb1f26c0ba858bc1616d262
Fabricio Di Paola <fabriciodamiandipaola@gmail.com>
2023-08-08 19:03:08 -0300

    ArticleDao-Atributtes-Completed

---
+++ ArticleAPI/Models/ArticleDao.cs
@@ -0,0 +1,14 @@
+﻿using DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ArticleAPI.Models
+{
+    public class ArticleDao
+    {
+        public ArticleDao() { data = new DataAccess(); }
+        public DataAccess data;
+    }
+}
