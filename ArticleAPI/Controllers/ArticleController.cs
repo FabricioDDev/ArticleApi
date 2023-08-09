349b00568d514739907e6a361d1c5952bb74e187
Fabricio Di Paola <fabriciodamiandipaola@gmail.com>
2023-08-09 08:04:00 -0300

    ArticleController-Atributtes-Created

---
+++ ArticleAPI/Controllers/ArticleController.cs
@@ -0,0 +1,16 @@
+﻿using ArticleAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ArticleAPI.Controllers
+{
+    public class ArticleController : ApiController
+    {
+        public ArticleController() { articleDao = new ArticleDao(); }
+        public ArticleDao articleDao;
+    }
+}
